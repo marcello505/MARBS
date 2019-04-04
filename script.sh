2e81cc0a7ca570296fca1f6e6bd55ee479c7278a
Marcello505 <marcellohaddeman@gmail.com>
2019-04-04 15:26:17 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,10 +10,35 @@ if yay -Syy
 then
 	echo "You're good to go."
 else
-	pacman -S --noconfirm --needed git
+	echo "Installing yay"
+	pacman -S --noconfirm --needed git base-devel
+	cd /tmp
 	git clone https://aur.archlinux.org/yay.git
 	cd yay
 	makepkg -si
 	cd ..
-	rmdir -r yay
 fi
+
+
+#Adding user + home directory.
+#read -p "Please enter a name for a new user:" name
+#if useradd -m "$name"
+#then
+#	echo "Now enter your password."
+#	if passwd "$name"
+#	then
+#		echo "Great, onto the next step."
+#	else
+#		echo "Did you enter anything? Did you retype it right?"
+#		exit
+#	fi
+#else
+#	echo "Did you enter anything? Did you use illegal characters?"
+#	exit
+#fi
+
+#Enabling services
+systemctl enable sddm.service
+
+#Changing keyboard type
+localectl --no-convert set-x11-keymap us pc105 intl
